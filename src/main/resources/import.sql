c5be84c6eed84ff8f0fa5ee5e3d52b5b5008f3f5
thanhlh7 <thanhlh7@fsoft.com.vn>
2018-06-29 14:08:03 +0700

    update pass

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,8 +1,8 @@
 INSERT INTO role (id, name, description) VALUES (1, 'ROLE_USER', 'Standard User - Has no admin rights');
 INSERT INTO role (id, name, description) VALUES (2, 'ROLE_ADMIN', 'Admin User - Has permission to perform admin tasks');
 
-INSERT INTO user (id, password, username) VALUES (1, '$2a$04$nVTw7YUhQalKaUJNsCBu6emvRPxyCdl7NAMVIeV6qFhbn/.exdv12', 'user');
-INSERT INTO user (id, password, username) VALUES (2, '$2a$04$nVTw7YUhQalKaUJNsCBu6emvRPxyCdl7NAMVIeV6qFhbn/.exdv12', 'admin');
+INSERT INTO user (id, password, username) VALUES (1, '$2a$04$8TqKBZptUcfLVOB5yTNNP.2asbSLv8ET5tUuFMZJlivu472en2C3G', 'user');
+INSERT INTO user (id, password, username) VALUES (2, '$2a$04$8TqKBZptUcfLVOB5yTNNP.2asbSLv8ET5tUuFMZJlivu472en2C3G', 'admin');
 
 INSERT INTO user_role(user_id, role_id) VALUES(1,1);
 INSERT INTO user_role(user_id, role_id) VALUES(2,1);
